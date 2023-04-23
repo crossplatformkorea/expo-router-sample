2dfc4fb780e184176e5313d4cf43fa599e02e87e
hyochan <dooboolab@gmail.com>
2023-04-23 17:08:38 +0900

    build: upgrade expo sdk to 48

---
+++ app.config.ts
@@ -0,0 +1,36 @@
+import type {ConfigContext, ExpoConfig} from '@expo/config';
+
+export default ({config}: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'dooboo',
+  slug: 'dooboo-expo-router',
+  privacy: 'public',
+  platforms: ['ios', 'android', 'web'],
+  version: '1.0.0',
+  orientation: 'default',
+  icon: './assets/icon.png',
+  plugins: ['sentry-expo', 'expo-localization'],
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  web: {
+    bundler: 'metro',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  userInterfaceStyle: 'automatic',
+  ios: {
+    supportsTablet: true,
+    infoPlist: {
+      CFBundleAllowMixedLocalizations: true,
+    },
+  },
+  android: {
+    userInterfaceStyle: 'automatic',
+  },
+  description: 'Starter project from dooboo-cli.',
+});
